0ec82723c582739e18b47ce681671d5440388ef0
unknown <nss1208@naver.com>
2021-06-26 17:21:15 +0900

    test

---
+++ app/src/main/java/com/nam/healthapp/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.nam.healthapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("daa", "onResume")
+    }
+
+
+}
---
+++ app/src/main/java/com/nam/healthapp/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.nam.healthapp.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.nam.healthapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
